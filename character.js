b0dc37a508b7fc52b18ba4717e4e9fb68f11f90b
Nathaniel <yoshi.toturo@gmail.com>
2024-03-22 14:59:54 -0700

    Better walls + wall line

--- character.js
+++ character.js
@@ -24,21 +24,32 @@ class Character {
         fill(128);
       }
 
-      if (point1 || point2) {
-        // find the slope of the line between p1 and p2
-        let dx = point1.x - point2.x;
-        let dy = point1.y - point2.y;
-        let slope = dx / dy;
 
-        // get slope of the point with respect to p1
-        let pointSlope = (point1.x - this.positionVector.x) / (point1.y - this.positionVector.y);
+      // const canvas = document.getElementById("canvas");
+      // const ctx = canvas.getContext("2d");
+      // const image = document.getElementById("source");
 
-        if (pointSlope > slope){
-            fill(0,255,0);
-        } else {
-            fill(255,0,0);
-        }
-      }
+      // image.addEventListener("load", (e) => {
+      //   ctx.drawImage("Greg.png", this.positionVector.x, this.positionVector.y)
+      // });
+      
+      // drawImage("Greg.png", this.positionVector.x, this.positionVector.y);
+
+      // if (point1 || point2) {
+      //   // find the slope of the line between p1 and p2
+      //   let dx = point1.x - point2.x;
+      //   let dy = point1.y - point2.y;
+      //   let slope = dx / dy;
+
+      //   // get slope of the point with respect to p1
+      //   let pointSlope = (point1.x - this.positionVector.x) / (point1.y - this.positionVector.y);
+
+      //   if (pointSlope > slope){
+      //       fill(0,255,0);
+      //   } else {
+      //       fill(255,0,0);
+      //   }
+      // }
       
       ellipse(0, 0, this.sprite, this.sprite);
 
@@ -50,23 +61,45 @@ class Character {
       pop();
   }
 
-  isCollide(b) {
+  isCollide(position, b) {
     return !(
-        ((this.positionVector.y + this.velocityVector.y) < (b.positionVector.y)) ||
-        (this.positionVector.y + this.velocityVector.y > (b.positionVector.y + b.sizeVector.y)) ||
-        ((this.positionVector.x + this.velocityVector.x) < b.positionVector.x) ||
-        (this.positionVector.x + this.velocityVector.x > (b.positionVector.x + b.sizeVector.x))
+        ((position.y) < (b.positionVector.y)) ||
+        (position.y > (b.positionVector.y + b.sizeVector.y)) ||
+        ((position.x) < b.positionVector.x) ||
+        (position.x > (b.positionVector.x + b.sizeVector.x))
     );
 }
 
-  updateCharacter(walls){
+  updateCharacter(walls, point1, point2){
     var hit = false;
     if (walls.length > 0) {
       for (let i = 0; i < walls.length; i++) {
         // console.log(i)
-        if (this.isCollide(walls[i])) {
-          hit = true;
+        let movedPositionY = createVector(this.positionVector.x, this.positionVector.y + this.velocityVector.y);
+        if (this.isCollide(movedPositionY, walls[i])) {
+          this.velocityVector = createVector(this.velocityVector.x, 0)
         }
+
+        let movedPositionX = createVector(this.positionVector.x + this.velocityVector.x, this.positionVector.y);
+        if (this.isCollide(movedPositionX, walls[i])) {
+          this.velocityVector = createVector(0, this.velocityVector.y)
+        }
+      }
+    }
+
+    if (point1 || point2) {
+      // find the slope of the line between p1 and p2
+      let dx = point1.x - point2.x;
+      let dy = point1.y - point2.y;
+      let slope = dx / dy;
+
+      // get slope of the point with respect to p1
+      let pointSlope = (point1.x - (this.positionVector.x + this.velocityVector.x)) / (point1.y - (this.positionVector.y + this.velocityVector.y));
+
+      if (pointSlope > slope){
+          hit = true
+      // } else {
+      //     hit = true
       }
     }
 
